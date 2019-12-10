29866202309375540e970614717c2c73759da897
Mark Zieg <mark@zieg.com>
2019-12-10 10:46:40 -0500

    updated .m script

--- WasatchDemo.m
+++ WasatchDemo.m
@@ -1,8 +1,13 @@
 % load the DLL
-dll = NET.addAssembly('C:\Program Files (x86)\Wasatch Photonics\Wasatch.NET\WasatchNET.dll');
+if computer('arch') == 'win64'
+    dll = NET.addAssembly('C:\Program Files\Wasatch Photonics\Wasatch.NET\WasatchNET.dll');
+else
+    dll = NET.addAssembly('C:\Program Files (x86)\Wasatch Photonics\Wasatch.NET\WasatchNET.dll');
+end
 
 % get a handle to the Driver Singleton
 driver = WasatchNET.Driver.getInstance();
+fprintf('Using %s from MATLAB %s\n', driver.version, version);
 
 % enumerate any connected spectrometers
 numberOfSpectrometers = driver.openAllSpectrometers();
@@ -21,12 +26,7 @@ serialNum   = char(spectrometer.serialNumber);
 wavelengths = spectrometer.wavelengths;
 
 % display summary
-fprintf('Found %s %s with %d pixels (%.2f, %.2fnm)\n', 
-    modelName, 
-    serialNum, 
-    pixels, 
-    wavelengths(1), 
-    wavelengths(wavelengths.Length));
+fprintf('Found %s %s with %d pixels (%.2f, %.2fnm)\n', modelName, serialNum, pixels, wavelengths(1), wavelengths(wavelengths.Length));
 
 % get a spectrum
 spectrometer.integrationTimeMS = 100;
